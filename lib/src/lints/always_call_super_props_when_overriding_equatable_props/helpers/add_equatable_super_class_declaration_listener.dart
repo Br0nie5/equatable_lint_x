cada0e60f937436c74c2d77630f752daec51ca5b
Br0nie5 <159724184+Br0nie5@users.noreply.github.com>
2024-02-28 17:33:39 +0100

    fix(call super lint): only show lint in Equatable extended classes

--- lib/src/lints/always_call_super_props_when_overriding_equatable_props/helpers/add_equatable_super_class_declaration_listener.dart
+++ lib/src/lints/always_call_super_props_when_overriding_equatable_props/helpers/add_equatable_super_class_declaration_listener.dart
@@ -1,5 +1,6 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:equatable_lint_x/src/constants/equatable_constants.dart';
 import 'package:equatable_lint_x/src/helpers/get_equatable_props_expression_infos.dart';
 import 'package:equatable_lint_x/src/helpers/get_has_override_equatable_props_in_super_class.dart';
 
@@ -27,6 +28,16 @@ extension AddEquatableSuperClassDeclarationListener on LintRuleNodeRegistry {
           classNode.declaredElement!.supertype?.element;
 
       if (classSuperTypeElement == null) {
+        return;
+      }
+
+      const typeChecker = TypeChecker.fromName(
+        equatableClassName,
+        packageName: equatablePackageName,
+      );
+      final classType = classSuperTypeElement.thisType;
+
+      if (!typeChecker.isAssignableFromType(classType)) {
         return;
       }
 
